500b8ac8b7592bde4b18a8f5b8f217f9c838d569
RileyRat5x <35906066+RileyRat5x@users.noreply.github.com>
2021-12-06 01:15:40 -0800

    sanitation is done and separation of bits is done

--- daythree.py
+++ daythree.py
@@ -1,15 +1,39 @@
 
 
 
+def sanitize(input):
+    newinput = []
+    for item in range(len(input)):
+        newinput.append(input[item][:len(input[item])-1])
+    return newinput
 
-def gammaNum():
+
+def gammaNum(input):
+    output =0
+
+
 
     return
 
-def epsilonNum():
+def epsilonNum(input ):
+    output = 0
+
 
     return
 
 def separateBits():
-
+    file = open("inputdaythree1.txt", "r")
+    input = file.readlines()
+    file.close()
+    newinput = sanitize(input)
+    bits = [[""],[""],[""],[""],[""],[""],[""],[""],[""],[""],[""],[""]]
+    for row in range(len(newinput)):
+        for bit in range(len(newinput[row])):
+            bits[bit][0] += newinput[row][bit]
+    print(bits)
     return
+
+if __name__ == '__main__':
+    c = separateBits()
+    # epsilion = epsilonNum(c)
+    gamma = gammaNum(c)
