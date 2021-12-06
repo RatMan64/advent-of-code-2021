eedcc198400294d9e42e00ed408dccdba79193f1
RileyRat5x <35906066+RileyRat5x@users.noreply.github.com>
2021-12-05 22:32:58 -0800

    part one done

--- dayone.py
+++ dayone.py
@@ -2,9 +2,36 @@
 
 
 def NumOfIncrease():
-    File_object = open(r"/input/input1.txt")
+    File_object = open("input1.txt", "r")
+    input = File_object.readlines()
+    File_object.close()
+    numofincrease = 0
+    prevousnum = 0
+    firsttime = True
 
-
+    for depth in range(len(input)):
+        if firsttime == True:
+            i = input[depth]
+            firsttime = False
+            prevousnum = int(i[:len(i)-1])
+            continue
+        num = input[depth]
+        num = int(num[:len(num)-1])
+        if prevousnum < num:
+            numofincrease += 1
+            prevousnum = num
+            continue
+        else:
+            prevousnum = num
+            continue
+    print(numofincrease)
     return
 
+def slidingwindow():
+    File = open("input2.txt","r")
+
+
+
+if __name__ == '__main__':
+    slidingwindow()
 
